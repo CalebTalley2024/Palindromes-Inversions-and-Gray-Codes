14d7c13a22e6085c15f14cc8d409e394a011ffbb
Caleb Talley <86822049+CalebTalley2024@users.noreply.github.com>
2022-11-20 22:32:11 -0500

    Finished 2a

---
+++ src/Inversion.java
@@ -0,0 +1,23 @@
+public class Inversion {
+
+    // Naive sorting: brute force
+    static double [] testNums1 = {3,2,1};
+    static double [] testNums2 = {5,1,4,2,3,6,7,9,8};
+
+    public static int easyinversioncount(double[] nums){
+        int invertCounter = 0;
+    // itterate throught the list using nested for loop
+        for(int i = 0; i<nums.length;i++){
+            for(int j = i+1; j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    invertCounter++;
+                }
+            }
+
+        }
+        System.out.println(" for your array, you have " + invertCounter +" inversions");
+    // if we have an inversion, then add 1 to the counter
+
+        return invertCounter;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        // 1
+//        Palindrome.palindromecheck();
 
+        //2a
+        Inversion.easyinversioncount(Inversion.testNums1);
+        //2b
 
-// add Scanner
-// 1
-        Palindrome.palindromecheck();
+        //
 
-//        Palindrome.makeStringBasic("a#b#$ &%^@&#*#c;d;eFgH");
-A
     }
 }
